dc08022f89d153561a76df1aaef992d55059c46d
Kyle Gothman <kyle.gothman@gmail.com>
2023-01-27 20:10:02 -0600

    minor css changes to register card component

--- src/components/registerCard/RegisterCard.js
+++ src/components/registerCard/RegisterCard.js
@@ -7,7 +7,7 @@ import NavButton from '../ui/button/Button';
 export function RegisterCard() {
     return (
         <Center bg='yellow.50'>
-        <Box minW='390px' minH='555px' bg='#FFFFF0' className='card'>
+        <Box className='card'>
             <Box className="header">
                 <h2>Sign Up</h2>
             </Box>
