f2c56d3ca98f484e1170cf4b7e39a668d9c202b7
Kyle Gothman <kyle.gothman@gmail.com>
2023-02-04 20:22:14 -0600

    Server file updates

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use( express.json())
-
 
 
 app.listen(
