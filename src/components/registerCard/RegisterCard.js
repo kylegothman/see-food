e1629d402182c6a9d5fe56970f647bce529225da
Kyle Gothman <kyle.gothman@gmail.com>
2023-02-07 12:39:42 -0600

    added react hook form to register card

--- src/components/registerCard/RegisterCard.js
+++ src/components/registerCard/RegisterCard.js
@@ -1,36 +1,45 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 import './registerCard.css'
 import { Center } from '@chakra-ui/react';
 import { Box } from '@chakra-ui/react';
 import NavButton from '../ui/button/Button';
 
 export function RegisterCard() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = data => console.log(data);
+    console.log(errors);
+
     return (
-        <Center bg='yellow.50'>
-        <Box className='card'
-                       _hover={{ 
-                        transform: 'translateY(-4px) translateX(-2px)',
-                        backgroundColor: '#FCD035',
-                        shadow: '2px 5px 0 0 black' }}>
-            <Box className="header">
-                <h2>Sign Up</h2>
-            </Box>
-            <Box className="inputBox">
-                <input className="name__input" type="text" name="name" autoComplete="off" required />
-                <label className="name__label" htmlFor="name">name</label>
-            </Box>
-            <Box className="inputBox">
-                <input className="username__input" type="text" name="name" autoComplete="off" required />
-                <label className="username__label" htmlFor="username">username</label>
-            </Box>
-            <Box className="inputBox">
-                <input className="pw__input" type="text" name="name" autoComplete="off" required />
-                <label className="pw__label" htmlFor="password">password</label>
-            </Box>
-            <Box p={7}>
-            <NavButton name='submit' />
-            </Box>
-        </Box>
-        </Center>
-    )
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Center bg='yellow.50'>
+                <Box className='card'
+                            _hover={{ 
+                                transform: 'translateY(-4px) translateX(-2px)',
+                                backgroundColor: '#FCD035',
+                                shadow: '2px 5px 0 0 black' }}>
+                    <Box className="header">
+                        <h2>Sign Up</h2>
+                    </Box>
+                    <Box className="inputBox">
+                        <input className="name__input" type="text" name="name" autoComplete="off" required {...register("name")}/>
+                        <label className="name__label" htmlFor="name">name</label>
+                    </Box>
+                    <Box className="inputBox">
+                        <input className="username__input" type="text" name="username" autoComplete="off" required {...register("username")}/>
+                        <label className="username__label" htmlFor="username">username</label>
+                    </Box>
+                    <Box className="inputBox">
+                        <input className="pw__input" type="text" name="pw" autoComplete="off" required {...register("pw")}/>
+                        <label className="pw__label" htmlFor="password">password</label>
+                    </Box>
+                    <Box p={7}>
+                    <NavButton type='submit' name='submit' />
+                    </Box>
+                </Box>
+                
+            </Center>
+        </form>
+    );
 }
