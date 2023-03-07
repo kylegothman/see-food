0f279de303c705eb7ee7415e5df8e51fbdc33b8c
Kyle Gothman <kyle.gothman@gmail.com>
2023-03-06 18:38:34 -0600

    added logo with moving eye component to the login page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form";
 import '../registerCard/registerCard.css';
 import { Center, Box, Button, Alert, AlertIcon, AlertDescription, } from '@chakra-ui/react';
 import theme from '../../themes/components/button.tsx';
+import './login.css'
+import LogoEyes from './LogoEyes';
 
 export default function Login({ onRouteChange, loadUser }) {
     const { register, handleSubmit, } = useForm();
@@ -36,34 +38,37 @@ export default function Login({ onRouteChange, loadUser }) {
     return (
         <Center bg='yellow.50'>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <Box className='loginCard' maxWidth={400} p={12}
+                <Box 
+                    className='loginCard' 
+                    maxWidth={400} p={12}
                     _hover={{ 
                         transform: 'translateY(-4px) translateX(-2px)',
                         backgroundColor: '#FCD035',
                         shadow: '2px 5px 0 0 black' }}>
-                    <Box className="header">
-                        <h2>Log In</h2>
-                    </Box>
-                    {alertMessage && <Alert mb={10} border='1px' borderRadius=".5em" status='error'>
-                                    <AlertIcon/>
-                                    <AlertDescription>{alertMessage}</AlertDescription>
-                                    </Alert>}
-                    <Box className="inputBox">
-                        <input className="username__input" type="text" name="username" autoComplete="off" required {...register("username", {maxLength: 20 })} onChange={handleInput}/>
-                        <label className="username__label" htmlFor="username">username</label>
-                    </Box>
-                    <Box className="inputBox">
-                        <input className="pw__input" type="password" name="password" autoComplete="off" required {...register("password")} onChange={handleInput}/>
-                        <label className="pw__label" htmlFor="password">password</label>
-                    </Box>
-                    <Button 
-                    theme={theme}
-                    mt={15}
-                    w='175px'
-                    type='submit' 
-                    name='submit'>
-                        Submit
-                    </Button>
+                        <LogoEyes/>
+                        <Box pt={1} className="header">
+                            <h2>Log In</h2>
+                        </Box>
+                        {alertMessage && <Alert mb={10} border='1px' borderRadius=".5em" status='error'>
+                                        <AlertIcon/>
+                                        <AlertDescription>{alertMessage}</AlertDescription>
+                                        </Alert>}
+                        <Box className="inputBox">
+                            <input className="username__input" type="text" name="username" autoComplete="off" required {...register("username", {maxLength: 20 })} onChange={handleInput}/>
+                            <label className="username__label" htmlFor="username">username</label>
+                        </Box>
+                        <Box className="inputBox">
+                            <input className="pw__input" type="password" name="password" autoComplete="off" required {...register("password")} onChange={handleInput}/>
+                            <label className="pw__label" htmlFor="password">password</label>
+                        </Box>
+                        <Button 
+                        theme={theme}
+                        mt={15}
+                        w='175px'
+                        type='submit' 
+                        name='submit'>
+                            Submit
+                        </Button>
                 </Box>
             </form>
         </Center>
---
+++ src/components/login/LogoEyes.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { Box } from "@chakra-ui/react";
+import "./login.css";
+import sf_logo from "../../assets/sf_logo.png";
+import logo_eyes from "../../assets/logo_eyes.png";
+
+export default function LogoEyes() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const onMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      setMousePosition({ x: clientX, y: clientY });
+    };
+
+    document.addEventListener("mousemove", onMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", onMouseMove);
+    };
+  }, []);
+
+  const anchorRef = React.useRef(null);
+
+  const anchorX = anchorRef.current?.getBoundingClientRect().left + anchorRef.current?.getBoundingClientRect().width / 2;
+  const anchorY = anchorRef.current?.getBoundingClientRect().top + anchorRef.current?.getBoundingClientRect().height / 2;
+ 
+  function angle(cx, cy, ex, ey) {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx);
+    const deg = rad * 180 / Math.PI;
+    return deg;
+  }
+
+  const angleDeg = angle(mousePosition.x, mousePosition.y, anchorX, anchorY);
+
+  return (
+    <Box className="main" >
+      <img id="anchor" ref={anchorRef} src={sf_logo} alt="logo" />
+      <Box id="eyes">
+        <img height='35px' width='35px' className="eye" src={logo_eyes} alt="pupil" style={{ top: 36, right: 69, transform: `rotate(${270 + angleDeg}deg)`}} />
+        <img height='35px' width='35px' className="eye" src={logo_eyes} alt="pupil" style={{ top: 36, right: 39, transform: `rotate(${270 + angleDeg}deg)` }} />
+      </Box>
+    </Box>
+  );
+}
